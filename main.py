7656c7aad0026adaabf01ceb99c1b453d982bf9c
Marco.Kuckart <marcokuckart@gmail.com>
2025-11-22 02:11:39 +0100

    json management

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from dotenv import load_dotenv
 import multiprocessing
 
 from src.bumper_selfbot import run_bump_task
+import src.json_manager as json_manager
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = 1396626329129975849
 
-bot_process = multiprocessing.Process(target=run_bump_task, args=(DISCORD_TOKEN, CHANNEL_ID))
-bot_process.start()
-bot_process.join()
+# bot_process = multiprocessing.Process(target=run_bump_task, args=(DISCORD_TOKEN, CHANNEL_ID))
+# bot_process.start()
+# bot_process.join()
 
-print("fini")
+json_manager.register_bot("853579408383803444", DISCORD_TOKEN, "moii")
+json_manager.register_channel(str(CHANNEL_ID), "pas de nom")
